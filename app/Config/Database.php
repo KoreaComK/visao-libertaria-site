d36d8313f61dfb5edf071e85bda45fc416256c4d
Koreacomk <koreacomk@protonmail.com>
2023-09-22 00:15:47 -0300

    Testando gitignore

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,7 +27,7 @@ class Database extends Config
     public array $default = [
         'DSN'      => '',
         'hostname' => 'localhost',
-        'username' => '',
+        'username' => 'root',
         'password' => '',
         'database' => 'visaolibertaria_teste',
         'DBDriver' => 'MySQLi',
