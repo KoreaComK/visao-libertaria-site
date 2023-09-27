c82be8c022661395999f05c2f0db3380dce9987b
KoreacomK <koreacomk@protonmail.com>
2023-09-27 15:00:46 -0300

    Corrigindo para prod

--- app/Config/Pager.php
+++ app/Config/Pager.php
@@ -24,7 +24,7 @@ class Pager extends BaseConfig
         'default_full'   => 'CodeIgniter\Pager\Views\default_full',
         'default_simple' => 'CodeIgniter\Pager\Views\default_simple',
         'default_head'   => 'CodeIgniter\Pager\Views\default_head',
-        'default_template'   => 'template\templatePaginacao',
+        'default_template'   => 'template/templatePaginacao',
     ];
 
     /**
