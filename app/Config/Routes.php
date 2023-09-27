397c75097ad1681e6382ba41ac602cea24eb4af1
KoreacomK <koreacomk@protonmail.com>
2023-09-27 15:09:32 -0300

    Configurando o index da prod

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,7 +29,7 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Site::index');
 
 /*
  * --------------------------------------------------------------------
