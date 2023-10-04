69afeeff873ad5efa04aca18cf8e97d78e758440
Koreacomk <koreacomk@protonmail.com>
2023-10-04 17:17:27 -0300

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/visao-libertaria-site/public';
+    public string $baseURL = 'http://localhost/visao-libertaria-site/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
