9a8a9ef08e96f98c98d9f09d446fdbdc2496960d
KoreaComK <86001769+KoreaComK@users.noreply.github.com>
2023-10-11 17:20:35 -0300

    Colocando o script

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -9,6 +9,7 @@
 	<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.min.js"></script>
+	<script src="https://js.hcaptcha.com/1/api.js" async defer></script>
 
 	<style type="text/css">
 		.vl-bg-c,
