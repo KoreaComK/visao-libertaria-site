a8fbb36536bbf2772ad028fabffbbd9e8bcfbdde
jesslsa <koreacomk@protonmail.com>
2024-01-11 17:27:51 -0300

    Simplificando tags de fechamento
    #89

--- app/Libraries/GerenciadorTextos.php
+++ app/Libraries/GerenciadorTextos.php
@@ -8,12 +8,24 @@ class GerenciadorTextos
 	public function contaPalavras($string = null)
 	{
 		$quantidade = 0;
-		if($string===null) {
+		if ($string === null) {
 			return false;
 		}
-		$quantidade = str_word_count(preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/","/(ç|Ç)/"),explode(" ","a A e E i I o O u U n N c"),$string));
+		$quantidade = str_word_count(preg_replace(array("/(á|à|ã|â|ä)/", "/(Á|À|Ã|Â|Ä)/", "/(é|è|ê|ë)/", "/(É|È|Ê|Ë)/", "/(í|ì|î|ï)/", "/(Í|Ì|Î|Ï)/", "/(ó|ò|õ|ô|ö)/", "/(Ó|Ò|Õ|Ô|Ö)/", "/(ú|ù|û|ü)/", "/(Ú|Ù|Û|Ü)/", "/(ñ)/", "/(Ñ)/", "/(ç|Ç)/"), explode(" ", "a A e E i I o O u U n N c"), $string));
 		return $quantidade;
 	}
 
-	
+	public function simplificaString($string, $caixaAlta = true): string
+	{
+		$string = trim($string);
+		$string = preg_replace(array("/(á|à|ã|â|ä)/", "/(Á|À|Ã|Â|Ä)/", "/(é|è|ê|ë)/", "/(É|È|Ê|Ë)/", "/(í|ì|î|ï)/", "/(Í|Ì|Î|Ï)/", "/(ó|ò|õ|ô|ö)/", "/(Ó|Ò|Õ|Ô|Ö)/", "/(ú|ù|û|ü)/", "/(Ú|Ù|Û|Ü)/", "/(ñ)/", "/(Ñ)/", "/(ç)/", "/(Ç)/"), explode(" ", "a A e E i I o O u U n N c C"), $string);
+		$string = preg_replace('/[^A-Za-z0-9\" "\-]/', '', $string);
+		if ($caixaAlta) {
+			$string = strtoupper($string);
+		} else {
+			$string = strtolower($string);
+		}
+		return $string;
+	}
+
 }
