c389159097b039792f5bbd941b8eab210b9800fd
Koreacomk <koreacomk@protonmail.com>
2024-02-15 07:10:23 -0300

    Squashed commit of the following:
    commit aed8701c1a1b25419d68059bb62450d4648dc37b
    Author: Koreacomk <koreacomk@protonmail.com>
    Date:   Thu Feb 15 07:06:01 2024 -0300
    
        Corrigindo a verificação para o de 7 dias também
    
    commit 250d9f73ed8a5a4aa29cadfca1ccc56d491b901b
    Author: Invencível <victor.guilherme100@hotmail.com>
    Date:   Wed Feb 14 20:09:19 2024 -0300
    
        Removendo testes unitarios. Ajuste espaços.
    
    commit 16f5a2c16887f8c227cd494c1f6bd5d4155f363a
    Author: Invencível <victor.guilherme100@hotmail.com>
    Date:   Tue Feb 13 01:58:59 2024 -0300
    
        Ajuste teste unitário e removendo comentário do código.
    
    commit ec118c9c8fa9b46839e272bd483365404eef6bc8
    Author: Invencível <victor.guilherme100@hotmail.com>
    Date:   Tue Feb 13 01:54:30 2024 -0300
    
        Reduzindo mudanças no código para deixar próximo do original.
    
    commit 265f871b64f59584286916332016ad68a29eb1ae
    Author: Invencível <victor.guilherme100@hotmail.com>
    Date:   Tue Feb 13 01:22:18 2024 -0300
    
        Ajustando lógica e testes unitários.
    
    commit 88144e6148e98c280e056b41f17fdca5a61ee8bd
    Author: Invencível <victor.guilherme100@hotmail.com>
    Date:   Tue Feb 13 00:39:16 2024 -0300
    
        Removendo import duplicado nos testes unitarios.
    
    commit 92558177b1ec7eb7b715588f93621b373c68ca4d
    Author: Invencível <victor.guilherme100@hotmail.com>
    Date:   Tue Feb 13 00:37:15 2024 -0300
    
        Ajustando lógica removendo variável ultimo_acesso que não existia.
    
    commit 08c52d16569e7038fe04b8bf8616fbf7daa2853e
    Author: Invencível <victor.guilherme100@hotmail.com>
    Date:   Tue Feb 13 00:14:26 2024 -0300
    
        Mudando reset de pautas diaria para dia atual e nao 24hrs.

--- app/Controllers/Colaboradores/Pautas.php
+++ app/Controllers/Colaboradores/Pautas.php
@@ -66,17 +66,17 @@ class Pautas extends BaseController
 
 			$session = $this->session->get('colaboradores');
 			
-			
-			$time = new Time('-1 days');
-			$time = $time->toDateTimeString();
-			$quantidade_pautas = $pautasModel->getPautasPorUsuario($time,$session['id'])[0]['contador'];
-			if($quantidade_pautas >= $data['config']['limite_pautas_diario']) {
+
+			$time = Time::today();
+			$time = $time->toDateString();
+			$quantidade_pautas = $pautasModel->getPautasPorUsuario($time, $session['id'])[0]['contador'];
+			if ($quantidade_pautas >= $data['config']['limite_pautas_diario']) {
 				$data['erros'] = $retorno->retorno(false, 'O limite diário de pautas foi atingido. Tente novamente amanhã.', false);
 				return view('colaboradores/pautas_form', $data);
 			}
 
 			$time = new Time('-7 days');
-			$time = $time->toDateTimeString();
+			$time = $time->toDateString();
 			$quantidade_pautas = $pautasModel->getPautasPorUsuario($time,$session['id'])[0]['contador'];
 			if($quantidade_pautas >= $data['config']['limite_pautas_semanal']) {
 				$data['erros'] = $retorno->retorno(false, 'O limite semanal de pautas foi atingido. Tente novamente outro dia.', false);
