c6cc84e6b5caecfa3d11500a5d4dc59a345e3afa
KoreaComK <86001769+KoreaComK@users.noreply.github.com>
2024-02-27 11:11:45 -0300

    Adicionando scroll na modal

--- app/Views/colaboradores/perfil.php
+++ app/Views/colaboradores/perfil.php
@@ -299,7 +299,7 @@ use CodeIgniter\I18n\Time;
 
 <div class="modal fade bd-example-modal-lg" id="modal-colaboracoes-fechadas" tabindex="-1" role="dialog"
 	aria-labelledby="modal-perfilLabel" aria-hidden="true">
-	<div class="modal-dialog modal-lg" role="document">
+	<div class="modal-dialog modal-lg modal-dialog-scrollable" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
 				<h5 class="modal-title" id="modal-perfilLabel">Colaborações já pagas</h5>
