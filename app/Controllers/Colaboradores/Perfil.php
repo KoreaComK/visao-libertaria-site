863ae0d40a8bc236aedb66dbf0c8ce57d6d45143
Koreacomk <koreacomk@protonmail.com>
2024-08-02 16:15:52 -0300

    Verificação do endereço pra ver se é de bitcoin

--- app/Controllers/Colaboradores/Perfil.php
+++ app/Controllers/Colaboradores/Perfil.php
@@ -33,6 +33,9 @@ class Perfil extends BaseController
 
 				$post['twitter'] = $gerenciadorTextos->simplificaString($post['twitter']);
 				$valida = $validaFormularios->validaFormularioPerfilColaborador($post, $session['id']);
+				if(substr($post['carteira'],0,1) != '1' && substr($post['carteira'],0,1) != '3' && substr($post['carteira'],0,3) != 'bc1') {
+					return $retorno->retorno(false, 'Perfil não atualizado! Endereço bitcoin inválido.', true);
+				}
 				if (empty($valida->getErrors())) {
 
 					$colaboradoresModel = new \App\Models\ColaboradoresModel();
